c8a53f11516475e3618cd8ea222dcb179f3b3943
Usman Musa <usmanmusa1920@gmail.com>
2022-12-04 22:31:12 +0100

    improved app default files datas

--- base.py
+++ base.py
@@ -38,12 +38,33 @@ class BaseStructure:
   def __init__(self, flaskey_software=False):
     """base structure class initializer"""
     self.flaskey_software = flaskey_software
-  fls_cmd = "touch"
+    self.fls_cmd = "touch"
+    self._exs_first = ["index", "style"]
+    self._exs_last = [".html", ".css", ".js"]
     
     
-  def append_exs_to_file(self, fls_name):
-    """append .py extension for files"""
-    return [i+".py" for i in fls_name]
+  def append_exs_to_file(self, fls_name=False, _exs_=".py", name=None):
+    """append .py extension for files if _exs_ value is ".py" or type is str, else if _exs_ type is list, make list of static files `['index.html', 'index.js', 'style.css']` """
+    
+    if type(_exs_) == list:
+      name, exst = self._exs_first, self._exs_last
+      lst = []
+      
+      for i in exst:
+        if i == exst[1]: # css
+          idx = name[1]
+          lst.append(f"{idx}{i}")
+        if i == exst[0] or i == exst[2]: # html & js
+          for j in exst:
+            if j != exst[1]:
+              idx = name[0]
+              if f"{idx}{j}" in lst:
+                pass
+              else:
+                lst.append(f"{idx}{j}")
+      return lst
+    if type(_exs_) == str:
+      return [i+f"{_exs_}" for i in fls_name]
   
   
   def file_content(self, _exs=False, content=None, file_name="index", dir_togo=None, route_go=True):
@@ -83,7 +104,7 @@ class BaseStructure:
     
     dirs = [proj_name, f"{proj_name}/{proj_name}", "templates", "static"]
     fls_name = ["__init__", "config", "models", "routes", "tunder"]
-    fls = self.append_exs_to_file(fls_name)
+    fls = self.append_exs_to_file(fls_name=fls_name)
     _here = os.getcwd() # initial `cwd` where the project was 
     
     # check if the project already exist
@@ -136,23 +157,33 @@ class AppStructure(BaseStructure):
   """base structure class"""
   app_store_name = None
   
-  def into_file(self, fls, fls_cmd, file=None, app_default_dummy=app_default_dummy):
+  def into_file(self, fls, fls_cmd, file=None, app_default_dummy=app_default_dummy, is_static_file=False):
     """create files within current directory of '''self.file_opt()'''    """
     for _fls in fls:
       sp.run(shlex.split(f"{fls_cmd} {_fls}"))
-      self.file_content(file_name=_fls, content=f"# Your app {_fls} file\n\n{app_default_dummy}", route_go=False) # building app default files
+      if is_static_file:
+        self.file_content(file_name=_fls, content=f"{app_default_dummy}", route_go=False) # building app default files
+      else:
+        self.file_content(file_name=_fls, content=f"# from flaskey software, your app {_fls} file\n\n{app_default_dummy}", route_go=False) # building app default files
         
         
-  def app_static_and_template(self, _dir_=False, file=False, app=False, cmd=False, _here_=False):
+  def app_static_and_template(self, file_dummy, top_comment=False, _dir_=False, file=False, app=False, cmd=False, _here_=False):
     # _dir_ = "template or static"
     # file = ["index.html"]
     # app = app name
     # cmd = "touch"
     # _here_ = # initial `inside project folder` where the project was created
     
+    if top_comment == "html":
+      top_comment = f"<!-- {app} {file[0]} page -->\n\n"
+    if top_comment == "css":
+      top_comment = f"/* {app} {file[0]} file */\n\n"
+    if top_comment == "js":
+      top_comment = f"/* {app} {file[0]} file */\n\n"
+    
     self.file_opt(_dir_, tree=False, _here=_here_) # back to template dir
     self.file_opt(f"{app}", _where=app) # make app dir inside `template`
-    self.into_file(file, cmd, app_default_dummy=_html) # making app default file
+    self.into_file(file, cmd, is_static_file=True, app_default_dummy=f"{top_comment}{file_dummy}") # making app default file
     
     
   def dir_tree(self, proj_app_name=None):
@@ -161,7 +192,7 @@ class AppStructure(BaseStructure):
     dirs = [proj_app_name]
     app_store_name = proj_app_name # store our app name
     fls_name = ["__init__", "views", "models", "routes"]
-    fls = self.append_exs_to_file(fls_name)
+    fls = self.append_exs_to_file(fls_name=fls_name)
     roove_dir = ["templates", "static/css", "static/js"]
     _here_app = os.getcwd()  # initial `inside project folder` where the project was created
     
@@ -179,15 +210,15 @@ class AppStructure(BaseStructure):
           self.into_file(fls, self.fls_cmd) # to maker app default files
           
       self.app_static_and_template(
-        _dir_=roove_dir[0], file=["index.html"], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
+        _html, top_comment="html", _dir_=roove_dir[0], file=[self.append_exs_to_file(_exs_=[])[0]], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
         )
       
       self.app_static_and_template(
-        _dir_=roove_dir[1], file=["style.css"], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
+        _css, top_comment="css", _dir_=roove_dir[1], file=[self.append_exs_to_file(_exs_=[])[2]], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
         )
       
       self.app_static_and_template(
-        _dir_=roove_dir[2], file=["index.js"], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
+        _js, top_comment="js", _dir_=roove_dir[2], file=[self.append_exs_to_file(_exs_=[])[1]], app=proj_app_name, cmd=self.fls_cmd, _here_=_here_app
         )
       
       self.file_opt("do_nothing", tree=False, _where=_here_app) # back to project dir
