bbaa45369a708e0d47aa5836ae9dcf151a6317c6
Usman Musa <usmanmusa1920@gmail.com>
2022-12-17 18:58:12 +0100

    added `test.py` module (for testing while development locally)

---
+++ test.py
@@ -0,0 +1,18 @@
+
+import flaskey
+from flaskey import project
+
+
+# print(help(flaskey))
+
+
+# initiate project on terminal
+# python -c "from flaskey import project; project('kyaah')"
+
+
+# to initiate a project
+project("kyaah")
+
+
+# to build an app, cd into your project dir that you create, and type:
+# python thunder.py create_app --app blog_app
