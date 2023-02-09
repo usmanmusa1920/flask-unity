83b59a1afdf600ae01b7ead11c26faa3de782b03
Usman Musa <usmanmusa1920@gmail.com>
2023-02-09 15:54:36 +0100

    rename an app default model and form class from dynamic to static (remove the app name prefix)

--- sakyum/dummy.py
+++ sakyum/dummy.py
@@ -502,10 +502,12 @@ from wtforms.validators import DataRequired, Length
 
 
 class {app_name.capitalize()}_QuestionForm(FlaskForm):
+  {long_comment} {app_name.capitalize()} default Question form {long_comment}
   question_text = TextAreaField('Question_Text', validators=[DataRequired()])
   submit = SubmitField('create')
 
 class {app_name.capitalize()}_ChoiceForm(FlaskForm):
+  {long_comment} {app_name.capitalize()} default Choice form {long_comment}
   question_id = StringField('Question_Id', validators=[DataRequired()])
   choice_text = StringField('Choice_Text', validators=[DataRequired(), Length(min=2, max=20)])
   submit = SubmitField('create')
@@ -521,23 +523,25 @@ when ever you create a model, make sure you import it in your
 project config.py file before you run your application to avoid error
 {long_comment}
 
-class {app_name.capitalize()}_QuestionModel(db.Model):
+class QuestionModel(db.Model):
+  {long_comment} {app_name.capitalize()} default Question model {long_comment}
   id = db.Column(db.Integer, primary_key=True)
   date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
   question_text = db.Column(db.Text, nullable=False)
-  choices = db.relationship('Choice', backref='selector', lazy=True)
+  choices = db.relationship('ChoiceModel', backref='selector', lazy=True)
 
   def __repr__(self):
     return f"Question number ('{f1}self.id{l1}', posted on: '{f1}self.date_posted{l1}')"
     
-  # the `Choice` is the choice model class below
+  # the `ChoiceModel` is the choice model class below
   # the `selector` is the attribute that we can use to get selector who choose the choice
   # the `lazy` argument just define when sqlalchemy loads the data from the database
 
-class {app_name.capitalize()}_ChoiceModel(db.Model):
+class ChoiceModel(db.Model):
+  {long_comment} {app_name.capitalize()} default Choice model {long_comment}
   id = db.Column(db.Integer, primary_key=True)
   date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+  question_id = db.Column(db.Integer, db.ForeignKey('question_model.id'), nullable=False)
   # you can pass a keyword argument of `unique=True` in the below choice_text field
   # that will make it unique across the entire table of choice
   choice_text = db.Column(db.String(100), nullable=False)
@@ -552,7 +556,7 @@ class {app_name.capitalize()}_ChoiceModel(db.Model):
 
 
 from {your_application}.config import db
-from todo_app.models import {app_name.capitalize()}_QuestionModel, {app_name.capitalize()}Choice
+from todo_app.models import QuestionModel, ChoiceModel
 
 
 # :method to create the tables and database, if it doesn't create db file,
@@ -560,23 +564,23 @@ run the below command. But if it create just ignore
 db.create_all()
 
 
-q1 = {app_name.capitalize()}_QuestionModel(question_text="Is sakyum an extension of flask web framework?")
-q2 = {app_name.capitalize()}_QuestionModel(question_text="Is flask better with sakyum")
+q1 = QuestionModel(question_text="Is sakyum an extension of flask web framework?")
+q2 = QuestionModel(question_text="Is flask better with sakyum")
 
 db.session.add(q1)
 db.session.add(q2)
 db.session.commit()
 
-the_q1 = {app_name.capitalize()}_QuestionModel.query.get_or_404(1)
-the_q2 = {app_name.capitalize()}_QuestionModel.query.get_or_404(2)
+the_q1 = QuestionModel.query.get_or_404(1)
+the_q2 = QuestionModel.query.get_or_404(2)
 
-c1_1 = {app_name.capitalize()}_ChoiceModel(choice_text="Yes, it is", question_id=the_q1.id)
-c1_2 = {app_name.capitalize()}_ChoiceModel(choice_text="No, it is not", question_id=the_q1.id)
-c1_3 = {app_name.capitalize()}_ChoiceModel(choice_text="I don't no", question_id=the_q1.id)
+c1_1 = ChoiceModel(choice_text="Yes, it is", question_id=the_q1.id)
+c1_2 = ChoiceModel(choice_text="No, it is not", question_id=the_q1.id)
+c1_3 = ChoiceModel(choice_text="I don't no", question_id=the_q1.id)
 
-c2_1 = {app_name.capitalize()}_ChoiceModel(choice_text="Yes for sure", question_id=the_q2.id)
-c2_2 = {app_name.capitalize()}_ChoiceModel(choice_text="Always the best", question_id=the_q2.id)
-c2_3 = {app_name.capitalize()}_ChoiceModel(choice_text="All the time", question_id=the_q2.id)
+c2_1 = ChoiceModel(choice_text="Yes for sure", question_id=the_q2.id)
+c2_2 = ChoiceModel(choice_text="Always the best", question_id=the_q2.id)
+c2_3 = ChoiceModel(choice_text="All the time", question_id=the_q2.id)
 
 db.session.add(c1_1)
 db.session.add(c1_2)
@@ -589,17 +593,17 @@ db.session.add(c2_3)
 db.session.commit()
 
 # to see all our questions
-{app_name.capitalize()}_QuestionModel.query.all()
-dir({app_name.capitalize()}_QuestionModel.query) # to see many other method
+QuestionModel.query.all()
+dir(QuestionModel.query) # to see many other method
 
 # to see choices related to our question number 1
-{app_name.capitalize()}_QuestionModel.query.get_or_404(1).choices
+QuestionModel.query.get_or_404(1).choices
 
 # to see all our choices
-{app_name.capitalize()}_ChoiceModel.query.all()
-dir({app_name.capitalize()}_ChoiceModel.query) # to see many other method
+ChoiceModel.query.all()
+dir(ChoiceModel.query) # to see many other method
 
-for i in {app_name.capitalize()}_ChoiceModel.query.all():
+for i in ChoiceModel.query.all():
   i.selector.question_text, i.choice_text
 
 {long_comment}
