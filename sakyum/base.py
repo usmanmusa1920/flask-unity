dea31255b52f030acc595ccc3c7ea562aecf711e
Usman Musa <usmanmusa1920@gmail.com>
2023-06-21 20:12:15 +0100

    start testing windows os

--- sakyum/base.py
+++ sakyum/base.py
@@ -56,6 +56,26 @@ class BaseStructure:
     self.fls_cmd = 'touch'
     self._exs_first = ['index', 'style']
     self._exs_last = ['.html', '.css', '.js']
+
+'''
+class BaseStructure:
+    """base structure class"""
+
+    def __init__(self, is_software=True):
+        """base structure class initializer"""
+        self.is_software = is_software
+        self.fls_cmd = 'echo. >'
+        self._exs_first = ['index', 'style']
+        self._exs_last = ['.html', '.css', '.js']
+
+    def create_files(self, directory):
+        """Create files with specified names and extensions"""
+        os.makedirs(directory, exist_ok=True)
+        for name in self._exs_first:
+            for extension in self._exs_last:
+                file_path = os.path.join(directory, f"{name}{extension}")
+                os.system(f"{self.fls_cmd} {file_path}")
+'''
     
 
   def append_exs_to_file(self, fls_name=False, _exs_='.py', name=None):
